dce6d10931764ba68922794510207c3fcc32bf53
bruno <brurro67@gmail.com>
2025-04-08 13:03:20 -0300

    alteraçoes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Livewire\Admin\Deshboard;
+use App\Livewire\Admin\Funcionario\FuncionarioIndex;
 use App\Livewire\Auth\Login;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Login::class)->name('login');
 
 Route::get('admin/deshboard', Deshboard::class)->middleware(['auth', 'user_type:admin'])->name('admin.deshboard');
+
+Route::get('/admin/funcionario', FuncionarioIndex::class)->middleware('auth', 'user_type:admin')->name('admin.funcionario.index');
